e142f6b2cc1a7e961fb7af6984234eb40c868807
DESKTOP-9M0JDAO\Lenovo <vulephanquoc@gmail.com>
2023-10-25 14:20:19 +0700

    fi_ex3

--- Source/Core/Src/update7seg.c
+++ Source/Core/Src/update7seg.c
@@ -7,22 +7,39 @@
 
 #include "update7seg.h"
 #include "led7seg.h"
+#include"main.h"
 const int MAX_LED = 4;
 int index_led = 0;
 int led_buffer[4] = {5 , 2 , 3 , 4};
 void update7SEG(int index ) {
 	 switch (index) {
 	 case 0:
+		 HAL_GPIO_WritePin(E_0_GPIO_Port, E_0_Pin, RESET);
+		HAL_GPIO_WritePin(E_1_GPIO_Port, E_1_Pin, SET);
+		HAL_GPIO_WritePin(E_2_GPIO_Port, E_2_Pin, SET);
+		HAL_GPIO_WritePin(E_3_GPIO_Port, E_3_Pin, SET);
 		 display7SEG(led_buffer[index]);
 	 break ;
 	 case 1:
+		 HAL_GPIO_WritePin(E_0_GPIO_Port, E_0_Pin, SET);
+		HAL_GPIO_WritePin(E_1_GPIO_Port, E_1_Pin, RESET);
+		HAL_GPIO_WritePin(E_2_GPIO_Port, E_2_Pin, SET);
+		HAL_GPIO_WritePin(E_3_GPIO_Port, E_3_Pin, SET);
 		 display7SEG(led_buffer[index]);
 	 break ;
 	 case 2:
+		 HAL_GPIO_WritePin(E_0_GPIO_Port, E_0_Pin, SET);
+		HAL_GPIO_WritePin(E_1_GPIO_Port, E_1_Pin, SET);
+		HAL_GPIO_WritePin(E_2_GPIO_Port, E_2_Pin, RESET);
+		HAL_GPIO_WritePin(E_3_GPIO_Port, E_3_Pin, SET);
 		 display7SEG(led_buffer[index]);
 	 break ;
 	 case 3:
-		 display7SEG(led_buffer[index]);
+		 HAL_GPIO_WritePin(E_0_GPIO_Port, E_0_Pin, SET);
+		HAL_GPIO_WritePin(E_1_GPIO_Port, E_1_Pin, SET);
+		HAL_GPIO_WritePin(E_2_GPIO_Port, E_2_Pin, SET);
+		HAL_GPIO_WritePin(E_3_GPIO_Port, E_3_Pin, RESET);
+		display7SEG(led_buffer[index]);
 	 break ;
 	 default :
 	 break ;
